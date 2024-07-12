8f4891419c5e2be381f0c1b7761cb438f6a94448
Vasil <electricianv@gmail.com>
2024-07-12 12:58:23 +0100

    get

--- NewsPaper/NewsPaper/settings.py
+++ NewsPaper/NewsPaper/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 from django.db import models
 
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'NewsPaper.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'news/templates/news'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import New
+
 
 # Create your views here.
+def index(request):
+    news = New.objects.all()
+    return render(request, 'index.html', context={'news': news})
+
+def detail(request, slug):
+    new = New.objects.get(slug__iexact=slug)
+    return render(request, 'details.html', context={'new':new})
